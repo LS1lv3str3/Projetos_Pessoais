d33f6bea0f75007b8eb5c6ac26d098b5c518055c
Lucas <lrasilvestre__@outlook.com>
2024-06-12 19:48:00 +0100

    Inicio tutorial react

---
+++ react_page/1st-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./homePage/Home.jsx"
+
+function App() {
+  
+
+  return (
+    <Home></Home>
+  );
+}
+
+export default App
---
+++ react_page/1st-react-app/src/homePage/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"  
+import Food from "./Food.jsx"
+
+function Home(){
+
+    return (
+        <>
+            <Header></Header>
+            <Food></Food>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default Home
